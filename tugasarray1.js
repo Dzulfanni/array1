128995aeeb5c2de46df460ad0de9ea2cd1de07c7
Dzulfanni <belajarayok866@gmail.com>
2020-09-11 06:37:03 +0700

    first commit

---
+++ tugasarray1.js
@@ -0,0 +1,17 @@
+var lisDesaBantul = [
+    [
+        'Canden',
+        'Jentis',
+        'Bantul'
+    ],
+    [
+        'Caturharjo',
+        'Pandak',
+        'Bantul'
+    ],
+    [
+        'Cetan',
+        'Sriganding',
+    ]
+]
+console.log(lisDesaBantul[0][0]);
